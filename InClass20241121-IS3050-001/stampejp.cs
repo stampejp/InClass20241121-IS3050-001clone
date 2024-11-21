ec0568dd0ecdf72f75ad428bbf6cc6c875dbeed9
stampejp <stampejp@mail.uc.edu>
2024-11-21 11:52:19 -0500

    OSU will crush IU

---
+++ InClass20241121-IS3050-001/stampejp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InClass20241121_IS3050_001
+{
+    public class stampejp
+    {
+        public static void Hello()
+        {
+            Console.WriteLine("OSU will crush IU");
+        }
+    }
+}
